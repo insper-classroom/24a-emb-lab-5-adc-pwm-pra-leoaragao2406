e68077b85cdcf0f7a74c3823dc67b0dfeec9b08a
leoaragao2406 <leo1zsaragao@hotmail.com>
2024-04-10 16:53:17 +0000

    feito

--- main/main.c
+++ main/main.c
@@ -1,6 +1,3 @@
-/*
- * LED blink with FreeRTOS
- */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
@@ -12,6 +9,10 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define X_AXIS_CHANNEL 26
+#define Y_AXIS_CHANNEL 27
 
 QueueHandle_t xQueueAdc;
 
@@ -20,24 +21,79 @@ typedef struct adc {
     int val;
 } adc_t;
 
+void adc_setup() {
+    adc_init();
+    adc_gpio_init(X_AXIS_CHANNEL);
+    adc_gpio_init(Y_AXIS_CHANNEL);
+}
+
+void write_package(adc_t data) {
+    int val = data.val;
+    int msb = val >> 8;
+    int lsb = val & 0xFF ;
+
+    uart_putc_raw(uart0, data.axis);
+    uart_putc_raw(uart0, lsb);
+    uart_putc_raw(uart0, msb);
+    uart_putc_raw(uart0, -1);
+}
+
+int read_and_scale_adc(int axis) {
+    adc_select_input(axis);
+    int raw = adc_read();
+
+    int scaled_val = ((raw - 2048) / 8);
+
+    if ((scaled_val > -180) && (scaled_val < 180)) {
+        scaled_val = 0; // Apply deadzone
+    }
+
+    return scaled_val/16;
+}
+
+void x_task(void *p) {
+    adc_t data;
+    data.axis = 0; // X-axis
+
+    while (1) {
+        data.val = read_and_scale_adc(data.axis);
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
+
+void y_task(void *p) {
+    adc_t data;
+    data.axis = 1; // Y-axis
+
+    while (1) {
+        data.val = read_and_scale_adc(data.axis);
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
 
 void uart_task(void *p) {
     adc_t data;
-
+    
     while (1) {
-        xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        if (xQueueReceive(xQueueAdc, &data, portMAX_DELAY)) {
+            write_package(data);
+        }
     }
 }
 
 int main() {
     stdio_init_all();
+    adc_setup();
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
-    xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
+    xTaskCreate(x_task, "X Task", 256, NULL, 1, NULL);
+    xTaskCreate(y_task, "Y Task", 256, NULL, 1, NULL);
+    xTaskCreate(uart_task, "UART Task", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (true)
-        ;
+    while (true) ;
 }
